b60c095ad20d85b096bd9eac54ac5abb7b476622
CHSU <du_opfer@dukeck.at>
2022-11-11 08:53:53 +0100

    Testing Movement

--- ShootingStar 1YA CHSU/ShootingStar 1YA CHSU/Program.cs
+++ ShootingStar 1YA CHSU/ShootingStar 1YA CHSU/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Reflection;
 using System.Security.Cryptography.X509Certificates;
@@ -11,7 +12,7 @@ namespace ShootingStar_1YA_CHSU
     internal class Program
     {
 
-        
+
         // Spielfeld
         struct Spielfeld
         {
@@ -30,7 +31,7 @@ namespace ShootingStar_1YA_CHSU
             public bool bEnemyType_Low;
             public bool bEnemyType_High;
             public bool bEnemyType_Mid;
-           
+
 
 
         }
@@ -50,13 +51,13 @@ namespace ShootingStar_1YA_CHSU
             public char Head = 'O';
             public char LeftHBottom = '/';
             public char RightHBottom = '\\';
-            
+
 
 
         }
         class Gegner
         {
-          
+
             public int[] enemyPos_4 = new int[2] { 4, 39 };
             public int[] enemyPos_5 = new int[2] { 5, 39 };
 
@@ -72,12 +73,14 @@ namespace ShootingStar_1YA_CHSU
 
             // Werte Variablen
             int Points = 0;
+            short jump_cooldown = 0;
+            uint JumpTicks = 0;
             byte max_enemys = 3;
             const int gameWidth = 40;
             const int gameHeight = 7;
             uint gameTicks = 0;
             // 30 Leicht 1 Schwer ?
-            uint  gameMode = 30;
+            uint gameMode = 30;
             uint Takt = 1000;
 
             //Strukturen
@@ -90,7 +93,7 @@ namespace ShootingStar_1YA_CHSU
                 enemyTypeLow[i] = new Gegner();
                 enemyTypeLow[i].enemy_ID = i;
             }
-            
+
             // Klasse Spieler
             Player player = new Player();
 
@@ -139,7 +142,7 @@ namespace ShootingStar_1YA_CHSU
                     gamefield[player.playerPosition_Body[0], player.playerPosition_Body[1]].playerPos_Body = true;
                     gamefield[player.playerPosition_Head[0], player.playerPosition_Head[1]].playerPos_Head = true;
 
-                    gamefield[player.playerPosition_LeftH[0],player.playerPosition_LeftH[1]].LeftHBottom = true;
+                    gamefield[player.playerPosition_LeftH[0], player.playerPosition_LeftH[1]].LeftHBottom = true;
                     gamefield[player.playerPosition_RightH[0], player.playerPosition_RightH[1]].RightHBottom = true;
 
                 }
@@ -150,7 +153,7 @@ namespace ShootingStar_1YA_CHSU
             Console.WriteLine("'S' Drücken um Spiel zu Starten");
             char menueEingabe = Console.ReadKey().KeyChar;
 
-            switch(menueEingabe)
+            switch (menueEingabe)
             {
 
                 // Spiel Starten
@@ -164,13 +167,18 @@ namespace ShootingStar_1YA_CHSU
                         while (true)
                         {
 
-                            Jump(gamefield, player, gameHeight, gameWidth, enemyTypeLow, Points);
+                            jump_cooldown =  Jump(gamefield, player, gameHeight, gameWidth, enemyTypeLow, Points ,  jump_cooldown);
                             gameTicks = SpawnEnemys(enemyTypeLow, gamefield, gameTicks, Takt, gameMode, gameHeight, gameWidth, player, Points);
                             gameTicks++;
+                            
+                            // Zähle Punkte
                             if (gameTicks % 2000 == 0)
                             {
                                 Points++;
                             }
+
+                           
+
 
 
 
@@ -184,19 +192,19 @@ namespace ShootingStar_1YA_CHSU
             }
 
 
-           
+
 
         } // Main Ende
 
 
 
-        static void DisplayGame(Spielfeld[,] gamefield, int gameHeight, int gameWidth, Player player , Gegner[] enemyTypeLow , int Points)
+        static void DisplayGame(Spielfeld[,] gamefield, int gameHeight, int gameWidth, Player player, Gegner[] enemyTypeLow, int Points)
         {
             Console.WriteLine("Deine Punkte : {0}", Points);
 
             for (int i = 0; i < gameHeight; i++)
             {
-                
+
                 for (int x = 0; x < gameWidth; x++)
 
                 {
@@ -236,7 +244,7 @@ namespace ShootingStar_1YA_CHSU
                     }
                     // Works
 
-                    else if (gamefield[i,x].LeftHBottom == true)
+                    else if (gamefield[i, x].LeftHBottom == true)
                     {
                         Console.ForegroundColor = ConsoleColor.DarkRed;
                         Console.Write(player.LeftHBottom);
@@ -244,14 +252,14 @@ namespace ShootingStar_1YA_CHSU
 
                     }
 
-                    else if (gamefield[i,x].RightHBottom == true)
+                    else if (gamefield[i, x].RightHBottom == true)
                     {
                         Console.ForegroundColor = ConsoleColor.DarkRed;
                         Console.Write(player.RightHBottom);
-                        Console.ForegroundColor = ConsoleColor.White;  
+                        Console.ForegroundColor = ConsoleColor.White;
                     }
 
-                    else if (gamefield[i,x].bEnemyType_Low == true)
+                    else if (gamefield[i, x].bEnemyType_Low == true)
                     {
                         Console.Write('|');
                     }
@@ -268,119 +276,71 @@ namespace ShootingStar_1YA_CHSU
 
         }
 
-        static void Jump(Spielfeld[,] gamefield, Player player, int gameHeight, int gameWidth , Gegner[] enemyTypeLow , int Points)
+        static short Jump(Spielfeld[,] gamefield, Player player, int gameHeight, int gameWidth, Gegner[] enemyTypeLow, int Points ,  short jump_cooldown)
         {
+
+          
+
             if (Console.KeyAvailable)
             {
                 if (player.playerPosition_LeftF[0] <= 5)
                 {
-                    if (player.playerPosition_Head[0] >= 0)
+                    if (jump_cooldown >= 0 && jump_cooldown < 3)
                     {
-                        ConsoleKeyInfo Key = Console.ReadKey(true);
+                        
 
-                        if (Key.Key == ConsoleKey.W && player.playerPosition_Head[0] > 0)
+                        if (player.playerPosition_Head[0] >= 0)
                         {
-                            gamefield[player.playerPosition_LeftF[0], player.playerPosition_LeftF[1]].playerPos_LeftF = false;
-                            gamefield[player.playerPosition_RightF[0], player.playerPosition_RightF[1]].playerPos_RightF = false;
-                            gamefield[player.playerPosition_Body[0], player.playerPosition_Body[1]].playerPos_Body = false;
-                            gamefield[player.playerPosition_Head[0], player.playerPosition_Head[1]].playerPos_Head = false;
-                            gamefield[player.playerPosition_LeftH[0], player.playerPosition_LeftH[1]].LeftHBottom = false;
-                            gamefield[player.playerPosition_RightH[0], player.playerPosition_RightH[1]].RightHBottom = false;
+                            ConsoleKeyInfo Key = Console.ReadKey(true);
 
-
-                            player.playerPosition_LeftF[0]--;
-                            player.playerPosition_RightF[0]--;
-                            player.playerPosition_Body[0]--;
-                            player.playerPosition_Head[0]--;
-                            player.playerPosition_LeftH[0]--;
-                            player.playerPosition_RightH[0]--;
-
-                            gamefield[player.playerPosition_LeftF[0], player.playerPosition_LeftF[1]].playerPos_LeftF = true;
-                            gamefield[player.playerPosition_RightF[0], player.playerPosition_RightF[1]].playerPos_RightF = true;
-                            gamefield[player.playerPosition_Body[0], player.playerPosition_Body[1]].playerPos_Body = true;
-                            gamefield[player.playerPosition_Head[0], player.playerPosition_Head[1]].playerPos_Head = true;
-                            gamefield[player.playerPosition_LeftH[0], player.playerPosition_LeftH[1]].LeftHBottom = true;
-                            gamefield[player.playerPosition_RightH[0], player.playerPosition_RightH[1]].RightHBottom = true;
-
-                            if (player.playerPosition_LeftH[0] == 3)
+                            if (Key.Key == ConsoleKey.W && player.playerPosition_Head[0] > 0)
                             {
-                                player.LeftHBottom = '-';
-                                player.RightHBottom = '-';
-                            }
-                            if (player.playerPosition_LeftH[0] <3 )
-                            {
-                                player.LeftHBottom ='\\';
-                                player.RightHBottom = '/';
-                            }
-                            if (player.playerPosition_LeftH[0] > 3)
-                            {
-                                player.LeftHBottom = '/';
-                                player.RightHBottom = '\\';
+
+
+                                InitializePlayerMovement(gamefield, player, -1);
+                                Console.Clear();
+                                DisplayGame(gamefield, gameHeight, gameWidth, player, enemyTypeLow, Points);
+                              
+                                
+                                
+
                             }
 
-
-                            Console.Clear();
-                            DisplayGame(gamefield, gameHeight, gameWidth, player , enemyTypeLow , Points );
-                        }
-
-                        if (Key.Key == ConsoleKey.S && player.playerPosition_LeftF[0] < 5)
-                        {
-                            gamefield[player.playerPosition_LeftF[0], player.playerPosition_LeftF[1]].playerPos_LeftF = false;
-                            gamefield[player.playerPosition_RightF[0], player.playerPosition_RightF[1]].playerPos_RightF = false;
-                            gamefield[player.playerPosition_Body[0], player.playerPosition_Body[1]].playerPos_Body = false;
-                            gamefield[player.playerPosition_Head[0], player.playerPosition_Head[1]].playerPos_Head = false;
-                            gamefield[player.playerPosition_LeftH[0], player.playerPosition_LeftH[1]].LeftHBottom = false;
-                            gamefield[player.playerPosition_RightH[0], player.playerPosition_RightH[1]].RightHBottom = false;
+                            
 
 
-                            player.playerPosition_LeftF[0]++;
-                            player.playerPosition_RightF[0]++;
-                            player.playerPosition_Body[0]++;
-                            player.playerPosition_Head[0]++;
-                            player.playerPosition_LeftH[0]++;
-                            player.playerPosition_RightH[0]++;
 
-                            gamefield[player.playerPosition_LeftF[0], player.playerPosition_LeftF[1]].playerPos_LeftF = true;
-                            gamefield[player.playerPosition_RightF[0], player.playerPosition_RightF[1]].playerPos_RightF = true;
-                            gamefield[player.playerPosition_Body[0], player.playerPosition_Body[1]].playerPos_Body = true;
-                            gamefield[player.playerPosition_Head[0], player.playerPosition_Head[1]].playerPos_Head = true;
-                            gamefield[player.playerPosition_LeftH[0], player.playerPosition_LeftH[1]].LeftHBottom = true;
-                            gamefield[player.playerPosition_RightH[0], player.playerPosition_RightH[1]].RightHBottom = true;
-
-                            if (player.playerPosition_LeftH[0] == 3)
+                            if (Key.Key == ConsoleKey.S && player.playerPosition_LeftF[0] < 5)
                             {
-                                player.LeftHBottom = '-';
-                                player.RightHBottom = '-';
-                            }
-                            if (player.playerPosition_LeftH[0] < 3)
-                            {
-                                player.LeftHBottom = '\\';
-                                player.RightHBottom = '/';
-                            }
-                            if (player.playerPosition_LeftH[0] > 3)
-                            {
-                                player.LeftHBottom = '/';
-                                player.RightHBottom = '\\';
-                            }
+                                InitializePlayerMovement(gamefield, player, 1);
 
                                 Console.Clear();
-                            DisplayGame(gamefield, gameHeight, gameWidth, player, enemyTypeLow , Points);
+                                DisplayGame(gamefield, gameHeight, gameWidth, player, enemyTypeLow, Points); 
+                            }
                         }
-                    }
-                }
 
+                        
+                    }
+                   
+                    
+
+                   
+                }
+                 
             }
 
 
+            return 2;
+
         }
 
-        static uint SpawnEnemys(Gegner[] enemyTypeLow , Spielfeld[,] gamefield , uint gameTicks , uint Takt , uint gameMode , int gameHeight , int gameWidth , Player player , int points)
+        static uint SpawnEnemys(Gegner[] enemyTypeLow, Spielfeld[,] gamefield, uint gameTicks, uint Takt, uint gameMode, int gameHeight, int gameWidth, Player player, int points)
         {
-           
 
-             if(gameTicks == Takt*5)
+
+            if (gameTicks == Takt * 5)
             {
-                
+
                 gamefield[enemyTypeLow[0].enemyPos_5[0], enemyTypeLow[0].enemyPos_5[1]].bEnemyType_Low = false;
                 gamefield[enemyTypeLow[0].enemyPos_4[0], enemyTypeLow[0].enemyPos_4[1]].bEnemyType_Low = false;
 
@@ -390,8 +350,8 @@ namespace ShootingStar_1YA_CHSU
                 gamefield[enemyTypeLow[0].enemyPos_5[0], enemyTypeLow[0].enemyPos_5[1]].bEnemyType_Low = true;
                 gamefield[enemyTypeLow[0].enemyPos_4[0], enemyTypeLow[0].enemyPos_4[1]].bEnemyType_Low = true;
 
-               gameTicks = Takt;
-                
+                gameTicks = Takt;
+
                 // Delete Enemy and Restore at beginning
                 if (enemyTypeLow[0].enemyPos_5[1] == 0 || enemyTypeLow[0].enemyPos_4[1] == 0)
                 {
@@ -403,22 +363,65 @@ namespace ShootingStar_1YA_CHSU
                 }
 
                 Console.Clear();
-                DisplayGame(gamefield, gameHeight, gameWidth, player, enemyTypeLow , points);
+                DisplayGame(gamefield, gameHeight, gameWidth, player, enemyTypeLow, points);
                 return gameTicks;
 
             }
 
-             return gameTicks;
+            return gameTicks;
 
-            
 
-          
+
+
         }
 
         static bool isNumeric(string A)
         {
             return A.All(char.IsNumber);
         }
+
+        static void InitializePlayerMovement(Spielfeld[,] gamefield, Player player, int Height)
+        {
+            gamefield[player.playerPosition_LeftF[0], player.playerPosition_LeftF[1]].playerPos_LeftF = false;
+            gamefield[player.playerPosition_RightF[0], player.playerPosition_RightF[1]].playerPos_RightF = false;
+            gamefield[player.playerPosition_Body[0], player.playerPosition_Body[1]].playerPos_Body = false;
+            gamefield[player.playerPosition_Head[0], player.playerPosition_Head[1]].playerPos_Head = false;
+            gamefield[player.playerPosition_LeftH[0], player.playerPosition_LeftH[1]].LeftHBottom = false;
+            gamefield[player.playerPosition_RightH[0], player.playerPosition_RightH[1]].RightHBottom = false;
+
+
+            player.playerPosition_LeftF[0] += Height;
+            player.playerPosition_RightF[0] += Height;
+            player.playerPosition_Body[0] += Height;
+            player.playerPosition_Head[0] += Height;
+            player.playerPosition_LeftH[0] += Height;
+            player.playerPosition_RightH[0] += Height;
+
+            gamefield[player.playerPosition_LeftF[0], player.playerPosition_LeftF[1]].playerPos_LeftF = true;
+            gamefield[player.playerPosition_RightF[0], player.playerPosition_RightF[1]].playerPos_RightF = true;
+            gamefield[player.playerPosition_Body[0], player.playerPosition_Body[1]].playerPos_Body = true;
+            gamefield[player.playerPosition_Head[0], player.playerPosition_Head[1]].playerPos_Head = true;
+            gamefield[player.playerPosition_LeftH[0], player.playerPosition_LeftH[1]].LeftHBottom = true;
+            gamefield[player.playerPosition_RightH[0], player.playerPosition_RightH[1]].RightHBottom = true;
+
+            if (player.playerPosition_LeftH[0] == 3)
+            {
+                player.LeftHBottom = '-';
+                player.RightHBottom = '-';
+            }
+            if (player.playerPosition_LeftH[0] < 3)
+            {
+                player.LeftHBottom = '\\';
+                player.RightHBottom = '/';
+            }
+            if (player.playerPosition_LeftH[0] > 3)
+            {
+                player.LeftHBottom = '/';
+                player.RightHBottom = '\\';
+            }
+        }
+
+
 
     } // Class Ende
 } // Namespace Ende
