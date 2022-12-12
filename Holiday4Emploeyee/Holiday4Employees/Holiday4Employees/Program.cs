0b6062e0ec345c0ef85f609e4ff680716367b883
Student <studen@student.at>
2022-12-12 15:39:11 +0100

    Fixed Step 1 to 5 in Holidays

--- Holiday4Emploeyee/Holiday4Employees/Holiday4Employees/Program.cs
+++ Holiday4Emploeyee/Holiday4Employees/Holiday4Employees/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
 
 namespace Holiday4Employees
@@ -49,7 +50,7 @@ namespace Holiday4Employees
                             if (currentEmployees < maxEmployees)
                             {
                                 currentEmployees = CreateEmployee(DatabaseEmployee, currentEmployees, PersonnelNumbers);
-                                PersonnelNumbers++; 
+                                PersonnelNumbers++;
                             }
 
                             else if (currentEmployees >= maxEmployees)
@@ -59,7 +60,8 @@ namespace Holiday4Employees
 
                         }
                         break;
-                        // Mitarbeiter Löschen
+
+                    // Mitarbeiter Löschen
                     case '2':
                         {
                             Console.WriteLine("Den zu wünschend Kündigenden Mitarbeiter bitte die PersonalNummer Eingeben : ");
@@ -94,17 +96,61 @@ namespace Holiday4Employees
                         }
                         break;
 
+                    // Urlaub Eintragen
                     case '3':
+                        {
+                            Console.WriteLine("Bitte Ihre Personalnummer eingeben :");
+                            string pID = Console.ReadLine();
+                            pID = Convert.ToString(DatabaseEmployee.FindIndex(e => e.Id == Convert.ToInt32(pID)));
+                            Console.WriteLine("An welchem Tag soll dein Urlaub beginnen ? z.B 05");
+                            string hDay = Console.ReadLine();
+
+                            Console.WriteLine("In Welchem Monat ? z.B 07");
+                            string hMonth = Console.ReadLine();
+
+                            Console.WriteLine("Und in welchem Jahr ? z.B {0}", DateTime.Now.Year);
+                            string hYear = Console.ReadLine();
+
+                            DateTime Holidaybegin = new DateTime(Convert.ToInt32(hYear), Convert.ToInt32(hMonth), Convert.ToInt32(hDay));
+
+                            Console.WriteLine("Wie Lange soll dein urlaub gehen ?");
+                            string HoldayDuration = Console.ReadLine();
+                            DateTime HolidayEnd = Holidaybegin.AddDays(Convert.ToInt32(HoldayDuration));
+
+                            ushort Id = DatabaseEmployee[Convert.ToInt32(pID)].Id;
+                            string LastName = DatabaseEmployee[Convert.ToInt32(pID)].LastName;
+                            string FirstName = DatabaseEmployee[Convert.ToInt32(pID)].FirstName;
+                            DateTime Birthdate = DatabaseEmployee[Convert.ToInt32(pID)].Birthdate;
+                            int Age = DatabaseEmployee[Convert.ToInt32(pID)].Age;
+                            int Holidays = DatabaseEmployee[Convert.ToInt32(pID)].Holidays - Convert.ToInt32(HoldayDuration);
+
+                            DatabaseEmployee.RemoveAt(Convert.ToInt32(pID));
+                            DatabaseEmployee.Insert(Convert.ToInt32(pID), new Employee() { Id = Id, LastName = LastName, FirstName = FirstName, Birthdate = Birthdate, Age = Age, Holidays = Holidays });
+                       
+
+
+                        }
                         break;
 
+
+
+                    // Mitarbeiter Suchen
                     case '4':
+                        {
+                            string pID;
+                            do
+                            {
+                                Console.WriteLine("Bitte Geben sie Ihre Personalnummer ein um ihren PersonalDatensatz zu suchen");
+                                pID = Console.ReadLine();
+                            } while (!isNumeric(pID));
+                            int Buffer = showEmploye(DatabaseEmployee, Convert.ToInt32(pID));
+                        }
                         break;
 
                     // Alle Mitarbeiter anzeigen lassen / Fertig
                     case '5':
                         {
                             showEmployees(DatabaseEmployee);
-                            
 
                         }
                         break;
@@ -117,7 +163,7 @@ namespace Holiday4Employees
                         }
                         break;
 
-                       
+
                 }
                 Console.WriteLine();
                 Console.WriteLine("Zum Fortfahren Beliebige Taste Drücken");
@@ -125,19 +171,15 @@ namespace Holiday4Employees
                 Console.Clear();
             }
 
-
-
         }
 
-
-
-        static ushort CreateEmployee(List<Employee> DatabaseEmployee, ushort currentEmployees , ushort PersonnelNumbers)
+        static ushort CreateEmployee(List<Employee> DatabaseEmployee, ushort currentEmployees, ushort PersonnelNumbers)
         {
 
             string firstName = "";
             string lastName = "";
             int holidays;
-            
+
             DateTime birthdate = new DateTime(1999, 01, 01);
             DateTime timeNow = DateTime.Now;
             DateTime Test = DateTime.Now.AddYears(-1);
@@ -154,8 +196,8 @@ namespace Holiday4Employees
             else { holidays = 30; }
 
 
-           
-            
+
+
             DatabaseEmployee.Insert(currentEmployees, new Employee() { Id = PersonnelNumbers, FirstName = firstName, LastName = lastName, Birthdate = birthdate, Age = Age, Holidays = holidays });
             currentEmployees += 1;
             return currentEmployees;
@@ -218,7 +260,7 @@ namespace Holiday4Employees
 
                     do
                     {
-                        Console.WriteLine("Dein Geburtsdatum ist der : {0} Korrekt ? J/N ",Datestring);
+                        Console.WriteLine("Dein Geburtsdatum ist der : {0} Korrekt ? J/N ", Datestring);
                         answer = Console.ReadKey().KeyChar;
                         Console.WriteLine();
 
@@ -239,7 +281,6 @@ namespace Holiday4Employees
             }
 
         }
-
         static void showEmployees(List<Employee> DatabaseEmployee)
         {
             foreach (Employee e in DatabaseEmployee)
@@ -258,26 +299,24 @@ namespace Holiday4Employees
                 Console.WriteLine();
             }
         }
-
-        static int showEmploye(List<Employee> DatabaseEmployee , int index)
+        static int showEmploye(List<Employee> DatabaseEmployee, int index)
         {
-           
-                int PersonnelIndex = DatabaseEmployee.FindIndex(e => e.Id == index);
-                string Datestring = DatabaseEmployee[PersonnelIndex].Birthdate.ToLongDateString();
-                Console.WriteLine("Mitarbeiternummer : {0}", DatabaseEmployee[PersonnelIndex].Id);
-                Console.WriteLine("Name :" + "" + DatabaseEmployee[PersonnelIndex].FirstName + " " + DatabaseEmployee[PersonnelIndex].LastName);
-                Console.WriteLine("Geburtsjahr : " + Datestring);
-                Console.WriteLine("Alter : {0} ", DatabaseEmployee[PersonnelIndex].Age);
-                Console.WriteLine("Urlaubsanspruch : {0}", DatabaseEmployee[PersonnelIndex].Holidays);
-                Console.WriteLine();
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("-----------------------------------------");
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine();
-            return PersonnelIndex;
-            
-        }
 
+            int PersonnelIndex = DatabaseEmployee.FindIndex(e => e.Id == index);
+            string Datestring = DatabaseEmployee[PersonnelIndex].Birthdate.ToLongDateString();
+            Console.WriteLine("Mitarbeiternummer : {0}", DatabaseEmployee[PersonnelIndex].Id);
+            Console.WriteLine("Name :" + "" + DatabaseEmployee[PersonnelIndex].FirstName + " " + DatabaseEmployee[PersonnelIndex].LastName);
+            Console.WriteLine("Geburtsjahr : " + Datestring);
+            Console.WriteLine("Alter : {0} ", DatabaseEmployee[PersonnelIndex].Age);
+            Console.WriteLine("Urlaubsanspruch : {0}", DatabaseEmployee[PersonnelIndex].Holidays);
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("-----------------------------------------");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+            return PersonnelIndex;
+
+        }
         static bool isNumeric(string? text)
         {
             return text.All(char.IsNumber);
