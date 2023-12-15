d5f9814dc028fcc1423bbd420ecc2965299ab126
Crypet_School <test@123.at>
2023-12-15 12:35:51 +0100

    ASP.Net User Login

---
+++ SchoolSecond/Firebase - User Login/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Firebase___User_Login.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
